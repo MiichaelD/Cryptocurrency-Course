5fa281dd3a5fb1693bfc7854561431b8599cb0de
Michael Duarte <michael.duarte@hotmail.com>
2018-01-13 14:51:51 -0800

    Added assignment 2 solution

--- src/assignment2starterCode/CompliantNode.java
+++ src/assignment2starterCode/CompliantNode.java
@@ -1,28 +1,65 @@
 package assignment2starterCode;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
-    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+    private double p_graph;
+    private double p_malicious;
+    private double p_tXDistribution;
+    private int numRounds;
+    private boolean[] followees;
+    private boolean[] blackListed;
+    private Set<Transaction> pendingTransactions;
+
+    public CompliantNode(
+        double p_graph,
+        double p_malicious,
+        double p_txDistribution,
+        int numRounds) {
+      this.p_graph = p_graph;
+      this.p_malicious = p_malicious;
+      this.p_tXDistribution = p_txDistribution;
+      this.numRounds = numRounds;
+      pendingTransactions = new HashSet<>();
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+      this.followees = followees;
+      this.blackListed = new boolean[followees.length];
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+      this.pendingTransactions.addAll(pendingTransactions);
     }
 
+    /** Returns a set of pending transactions to be sent to this node's followers. */
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        Set<Transaction> toSend = pendingTransactions;
+        pendingTransactions = new HashSet<>();
+        return toSend;
     }
 
+    /**
+     * Checks if received set of candidate transactions come from known followees and adds them to
+     * the pending transaction set.
+     */
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        Set<Integer> senders = new HashSet<>();
+        for (Candidate candidate : candidates) {
+            senders.add(candidate.sender);
+        }
+
+        for (int i = 0; i < followees.length; ++i) {
+            if (followees[i] && !senders.contains(i))
+                blackListed[i] = true;
+        }
+        for (Candidate c : candidates) {
+            if (!blackListed[c.sender]) {
+                pendingTransactions.add(c.tx);
+            }
+        }
     }
 }
--- src/assignment2starterCode/MaliciousNode.java
+++ src/assignment2starterCode/MaliciousNode.java
@@ -1,27 +1,23 @@
 package assignment2starterCode;
 
-import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
 
 public class MaliciousNode implements Node {
 
-    public MaliciousNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-    }
+    public MaliciousNode(
+        double p_graph,
+        double p_malicious,
+        double p_txDistribution,
+        int numRounds) { }
 
-    public void setFollowees(boolean[] followees) {
-        return;
-    }
+    public void setFollowees(boolean[] followees) { }
 
-    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        return;
-    }
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) { }
 
     public Set<Transaction> sendToFollowers() {
-        return new HashSet<Transaction>();
+        return new HashSet<>();
     }
 
-    public void receiveFromFollowees(Set<Candidate> candidates) {
-        return;
-    }
+    public void receiveFromFollowees(Set<Candidate> candidates) { }
 }
